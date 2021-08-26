f607ff9061be26b5992846d452a91016f34e26ed
Johannes Frey-Skött <johannes@freyskott.com>
2021-08-26 04:28:32 +0300

    Adding more generic collector function. TODO: complete the function with the parsing of return data.

--- CryptoCollector.py
+++ CryptoCollector.py
@@ -3,12 +3,37 @@ import pandas as pd
 import requests
 import json
 
+data_dir = "./data"
+url_base = "https://api.kraken.com/0/public/"
+timeframes = {"minute": 1, "hour": 60, "day": 1440}
+
+def fetch_data(**kwargs):
+    """More generic function"""
+
+    symbol = kwargs.get('symbol', "BTC/USD")
+    data_type = kwargs.get('data_type', "OHLC") # OHLC, Spread, Trades
+    timeframe = kwargs.get('timeframe', "day")
+
+    pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/USD
+    symbol = pair_split[0] + pair_split[1]
+    if data_type == "OHLC":
+        url = f'{url_base}/OHLC?pair={symbol}&interval={timeframe}'
+    else:
+        url = f'{url_base}/{data_type}?pair={symbol}'
+
+    response = requests.get(url)
+    if response.status_code == 200:  # check to make sure the response from server is good
+        j = json.loads(response.text)
+        result = j['result']
+        keys = []
+        for item in result:
+            keys.append(item)
 
 def fetch_OHLC_data(symbol, timeframe):
     """This function will get Open/High/Low/Close, Volume and tradecount data for the pair passed and save to CSV"""
     pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/USD
     symbol = pair_split[0] + pair_split[1]
-    url = f'https://api.kraken.com/0/public/OHLC?pair={symbol}&interval={timeframe}'
+    url = f'{url_base}/OHLC?pair={symbol}&interval={timeframe}'
     response = requests.get(url)
     if response.status_code == 200:  # check to make sure the response from server is good
         j = json.loads(response.text)
@@ -38,7 +63,7 @@ def fetch_OHLC_data(symbol, timeframe):
                 tf = 'day'
             else:
                 tf = ''
-            data.to_csv(f'Kraken_{symbol}_{tf}.csv', index=False)
+            data.to_csv(f'{data_dir}/Kraken_{symbol}_{tf}.csv', index=False)
     else:
         print("Did not receieve OK response from Kraken API")
 
@@ -47,7 +72,7 @@ def fetch_SPREAD_data(symbol):
         the results to a CSV file"""
     pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/USD
     symbol = pair_split[0] + pair_split[1]
-    url = f'https://api.kraken.com/0/public/Spread?pair={symbol}'
+    url = f'{url_base}/Spread?pair={symbol}'
     response = requests.get(url)
     if response.status_code == 200:  # check to make sure the response from server is good
         j = json.loads(response.text)
@@ -67,7 +92,7 @@ def fetch_SPREAD_data(symbol):
         if data is None:
             print("Did not return any data from Kraken for this symbol")
         else:
-            data.to_csv(f'Kraken_{symbol}_spreads.csv', index=False)
+            data.to_csv(f'{data_dir}/Kraken_{symbol}_spreads.csv', index=False)
     else:
         print("Did not receieve OK response from Kraken API")
 
@@ -75,7 +100,7 @@ def fetch_PRINTS_data(symbol):
     """This function will return historical trade prints for the symbol passed and save the results to a CSV file"""
     pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/USD
     symbol = pair_split[0] + pair_split[1]
-    url = f'https://api.kraken.com/0/public/Trades?pair={symbol}'
+    url = f'{url_base}/Trades?pair={symbol}'
     response = requests.get(url)
     if response.status_code == 200:  # check to make sure the response from server is good
         j = json.loads(response.text)
@@ -99,18 +124,18 @@ def fetch_PRINTS_data(symbol):
         if data is None:
             print("Did not return any data from Kraken for this symbol")
         else:
-            data.to_csv(f'Kraken_{symbol}_tradeprints.csv', index=False)
+            data.to_csv(f'{data_dir}/Kraken_{symbol}_tradeprints.csv', index=False)
     else:
         print("Did not receieve OK response from Kraken API")
 
 
 if __name__ == "__main__":
     # we set which pair we want to retrieve data for
-    pair = "BTC/USD"
+    pair = "ALGO/USD"
     # full timeframe intervals found here: https://www.kraken.com/en-us/features/api#get-ohlc-data
-    fetch_OHLC_data(symbol=pair, timeframe='1') # fetches minute data
+    #fetch_OHLC_data(symbol=pair, timeframe='1') # fetches minute data
     # fetch_OHLC_data(symbol=pair, timeframe='60')  # fetches hourly data
-    # fetch_OHLC_data(symbol=pair, timeframe='1440')  # fetches daily data
+    fetch_OHLC_data(symbol=pair, timeframe='1440')  # fetches daily data
     fetch_SPREAD_data(symbol=pair) # gets bid/ask spread data
     fetch_PRINTS_data(symbol=pair) # gets historical trade print data
 
